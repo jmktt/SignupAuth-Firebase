4c62c639a43e85468ec13567cb74c7c6162c9e9c
João Mauricio Cardoso Gueiros <joaomcg0505@gmail.com>
2024-07-03 12:54:31 -0300

    .

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
-# SignupAuth-Firebase
-LoginSignupAuth - Kotlin Firebase
+## SignupAuth-Firebase
+
+This is an Android application developed in Kotlin that utilizes Firebase Authentication to manage user registration and login.
+
+### Features
+
+- **User Registration**: Allows new users to register using an email address and password.
+- **User Login**: Authentication for already registered users.
+- **Firebase Integration**: Secure storage of user data in the cloud
+
+### Prerequisites
+- Android device with API Level 21 or higher.
+- Firebase account to set up the project.
+
+### Setup
+1. Clone the repository.
+2. Set up your project in the [Firebase Console](https://console.firebase.google.com):
+   - Add the Android app to your Firebase project.
+   - Download the `google-services.json` file and add it to the `app/` directory of the project.
+3. Build and run the app in Android Studio.
+
+##
+<p align="center">
+  <img src=".github/login.png">
+</p>
+
+## Technologies Used
+
+- Kotlin: Official programming language for Android development.
+- Firebase Authentication: For securely managing user authentication.
+- Android Studio: Integrated Development Environment (IDE) for Android.
---
+++ app/src/main/java/com/example/loginsignupauth/LoginActivity.kt
@@ -0,0 +1,48 @@
+package com.example.loginsignupauth
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.loginsignupauth.databinding.ActivityLoginBinding
+import com.google.firebase.auth.FirebaseAuth
+
+class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
+    private lateinit var firebaseAuth: FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        firebaseAuth = FirebaseAuth.getInstance()
+
+        binding.loginButton.setOnClickListener{
+            val email = binding.loginEmail.text.toString()
+            val password = binding.loginPassword.text.toString()
+
+            if (email.isNotEmpty() && password.isNotEmpty()){
+
+                firebaseAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener{
+                    if (it.isSuccessful){
+                        var intent = Intent(this, MainActivity::class.java)
+                        startActivity(intent)
+                    } else {
+                        Toast.makeText(this, it.exception.toString(), Toast.LENGTH_SHORT).show()
+                    }
+                }
+            } else{
+                Toast.makeText(this, "Fields cannot be empty", Toast.LENGTH_SHORT).show()
+            }
+        }
+        binding.signupRedirectText.setOnClickListener {
+            val signupIntent = Intent(this, SignupActivity::class.java)
+            startActivity(signupIntent)
+        }
+    }
+}
